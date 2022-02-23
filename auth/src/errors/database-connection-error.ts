e9a27aedfc8c5e400f831e323e2512eccd0c9187
mayankjain0141 <f20190141@pilani.bits-pilani.ac.in>
2022-02-23 10:35:57 +0530

    errorHandler middleware: added response serialization

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,7 +1,12 @@
 export class DatabaseConnectionError extends Error {
+    statusCode = 500;
     reason = "Error connecting to database";
     constructor() {
         super();
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype); // Fixes bug for extending in-built-class
+    }
+
+    serializeErrors() {
+        return [{ message: this.reason }];
     }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,12 +1,18 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
-    errors: ValidationError[];
+    // errors: ValidationError[];
+    statusCode = 400;
 
-    constructor(errors: ValidationError[]) {
+    constructor(public errors: ValidationError[]) {
         super();
-        this.errors = errors;
-
+        // this.errors = errors;
         Object.setPrototypeOf(this, RequestValidationError.prototype); // Fixes bug for extending in-built-class
+    }
+
+    serializeErrors() {
+        return this.errors.map((err) => {
+            return { message: err.msg, field: err.param };
+        });
     }
 }

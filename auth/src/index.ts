70b5489c6b6a6981e0d8ef9d3967e2ecd39d0214
mayankjain0141 <f20190141@pilani.bits-pilani.ac.in>
2022-03-04 11:17:27 +0530

    added db k8s config and initialised mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
@@ -19,10 +20,22 @@ app.use(signoutRouter);
 
 app.use(errorHandler);
 
-app.all("*", async (req,res) => {
+app.all("*", async (req, res) => {
     throw new NotFoundError();
 });
+
+const start = async () => {
+    try{
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("connected to mongodb");
+        
+    }catch(err){
+        console.error(err);
+    }
+};
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
+
+start();

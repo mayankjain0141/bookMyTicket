fbebffbda0e3c13dff09d05813e8337acb130fcc
mayankjain0141 <f20190141@pilani.bits-pilani.ac.in>
2022-03-07 12:00:39 +0530

    fixed build failing bug: commas smh

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -34,8 +34,4 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
-const buildUser = (attrs: UserAttrs) => {
-    return new User(attrs);
-};
-
-export { User, buildUser };
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -20,10 +20,18 @@ router.post(
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            console.log("Email in use");
+            return res.send({});
+        }
+        // hash passwd
+        const user = User.build({ email, password });
+        // save to db
+        await user.save();
 
-        console.log("Creating a user...");
-        throw new DatabaseConnectionError(); // Just for testing
-        res.send({});
+        res.status(201).send(user);
     }
 );
 

40e38131389cd7e7dacffcabb507004009b6af35
mayankjain0141 <f20190141@pilani.bits-pilani.ac.in>
2022-04-14 13:53:05 +0530

    added tests for signout and current-user routes

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+// telling typescript that global property exists
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -23,3 +29,20 @@ afterAll(async () => {
     await mongoose.connection.close();
     await mongo.stop();
 });
+
+global.signin = async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const response = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email,
+            password,
+        })
+        .expect(201);
+
+    const cookie = response.get("Set-Cookie");
+
+    return cookie;
+};

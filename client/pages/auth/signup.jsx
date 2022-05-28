b019dcb362f3f5ef3be4c7e70613b02e39f7328e
mayankjain0141 <f20190141@pilani.bits-pilani.ac.in>
2022-05-28 12:27:33 +0530

    refactored error rendering into use-request hook

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 import { useState } from "react";
+import useRequest from "../../hooks/use-request";
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]); // Errors is an array
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method:'post',
+        body:{
+            email,password
+        }
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault(); // to prevent the form does not submit itself
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-            console.log(response.data);
-        } catch (err) {
-            console.log(err);
-        }
+        doRequest();
     };
 
     return (
@@ -37,13 +37,7 @@ export default () => {
                     type="password" className="form-control"
                 />
             </div>
-            {errors.length>0 && (<div className="alert alert-danger">
-                <h4>Oops...</h4>
-                <ul className="my-0">
-                    {errors.map(err => (
-                        <li key={err.message}>{err.message}</li>))}
-                </ul>
-            </div>)}
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
